3de719feeb42ccb119a6815b4db91fdcee2e01f9
RichaAgarwal30 <Agarwalricha480@gmail.com>
2019-07-27 14:36:31 +0530

    Input for a string variable with name student for the name of a student and print that name.

--- src/com/student/StudentInfo.java
+++ src/com/student/StudentInfo.java
@@ -1,8 +1,12 @@
 package com.student;
 
+import java.util.Scanner;
+
 public class StudentInfo {
     public static void main(String[] args) {
-        String Student = "Ram";
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a string :");
+        String Student = scn.nextLine();
         System.out.println("Student is =" + Student);
     }
 
