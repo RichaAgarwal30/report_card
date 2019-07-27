d6eedf91e3fe6ada3991d86cde68372bf4676c31
RichaAgarwal30 <Agarwalricha480@gmail.com>
2019-07-27 14:49:17 +0530

    Take 2 user input for a string variable with name student1 and student2 for the name of a student and print those names.

--- src/com/student/StudentInfo.java
+++ src/com/student/StudentInfo.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class StudentInfo {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.print("Enter a string :");
-        String Student = scn.nextLine();
-        System.out.println("Student is =" + Student);
+        System.out.print("Enter Student1 Name :");
+        String Student1 = scn.nextLine();
+        System.out.print("Enter Student2 Name :");
+        String Student2 = scn.nextLine();
+        System.out.println("Student1 is =" + Student1);
+        System.out.println("Student2 is =" + Student2);
     }
 
 }
