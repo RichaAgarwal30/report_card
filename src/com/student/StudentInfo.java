c93394b399f61e86e1cd6a9ec010f24f7880fbc5
RichaAgarwal30 <Agarwalricha480@gmail.com>
2019-07-27 14:06:22 +0530

    Variable of string type with name student for the name of a student and print that name

---
+++ src/com/student/StudentInfo.java
@@ -0,0 +1,9 @@
+package com.student;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        String Student = "Ram";
+        System.out.println("Student is =" + Student);
+    }
+
+}
