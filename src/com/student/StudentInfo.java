ed756934644bfad9da339dc87dc6debd10169cc1
RichaAgarwal30 <Agarwalricha480@gmail.com>
2019-07-27 18:30:20 +0530

    Report Card Inputting Marks for 2 students and 3 subjects: Grade of only 1 student

--- src/com/student/StudentInfo.java
+++ src/com/student/StudentInfo.java
@@ -31,16 +31,21 @@
 
 package com.student;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
 
 public class StudentInfo {
     public static void main(String[] args) throws IOException {
 
-        String Subject1 = "English", Subject2 = "Hindi", Subject3 = "Maths";
+        String Subject1 = "English";
+        String Grade = null;
+        float E1;
 
+        /*String Subject2 = "Hindi";
+        String Subject3 = "Maths";
+
+*/
         BufferedReader br = new BufferedReader(new
                 InputStreamReader(System.in));
 
@@ -48,8 +53,9 @@ public class StudentInfo {
         String Student1 = br.readLine();
 
         System.out.print("Enter Marks in " + Subject1 + ":");
-        float E1 = Float.parseFloat(br.readLine());
-        System.out.print("Enter Marks in " + Subject2 + ":");
+        E1 = Float.parseFloat(br.readLine());
+
+        /*System.out.print("Enter Marks in " + Subject2 + ":");
         float H1 = Float.parseFloat(br.readLine());
         System.out.print("Enter Marks in " + Subject3 + ":");
         float M1 = Float.parseFloat(br.readLine());
@@ -63,15 +69,32 @@ public class StudentInfo {
         System.out.print("Enter Marks in " + Subject2 + ":");
         float H2 = Float.parseFloat(br.readLine());
         System.out.print("Enter Marks in " + Subject3 + ":");
-        float M2 = Float.parseFloat(br.readLine());
+        float M2 = Float.parseFloat(br.readLine());*/
+
+        if (E1 >= 90 && E1 <= 100) {
+            Grade = "A+";
+        } else if (E1 >= 80 && E1 <= 89) {
+            Grade = String.valueOf('A');
+        } else if (E1 >= 70 && E1 <= 79) {
+            Grade = "B+";
+        } else if (E1 >= 60 && E1 <= 69) {
+            Grade = String.valueOf('B');
+        } else if (E1 >= 50 && E1 <= 59) {
+            Grade = "C+";
+        } else if (E1 >= 40 && E1 <= 49) {
+            Grade = String.valueOf('C');
+        } else if (E1 >= 0 && E1 <= 40) {
+            Grade = String.valueOf('F');
+        }
 
         System.out.println("----------------------");
         System.out.println("Student1 :" + Student1);
         System.out.println("----------------------");
-        System.out.println("Subjects" + "  " + "Marks");
+        System.out.println("Subjects" + "  " + "Marks" + "  " + "Grade");
 
-        System.out.println(Subject1 + "   " + E1);
-        System.out.println(Subject2 + "     " + H1);
+        System.out.println(Subject1 + "   " + E1 + "    " + Grade);
+
+        /*System.out.println(Subject2 + "     " + H1);
         System.out.println(Subject3 + "     " + M1);
 
         System.out.println("----------------------");
@@ -83,7 +106,7 @@ public class StudentInfo {
         System.out.println(Subject1 + "   " + E2);
         System.out.println(Subject2 + "     " + H2);
         System.out.println(Subject3 + "     " + M2);
-
+*/
     }
 
 }
